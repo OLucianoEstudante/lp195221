e4003d307a9e1e5f853e09531d4c026d2cd6c0da
OLucianoEstudante <luciano.netto@unifesp.br>
2026-04-16 12:21:16 -0300

    Update 2757.c

--- 2757.c
+++ 2757.c
@@ -1,9 +1,10 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Luciano de Andrade da Silva Netto
 Linguagem   : C
 Plataforma  : https://judge.beecrowd.com/pt/search
 Exercício   : 2757
-Data        : 15/04/2026
+Data        : 16/04/2026
 Objetivo    : outputs de quatro variaveis, mostrando diferentes formas de como apresenta-la
 Aprendizado : especificacao de formato (adicionar espacos e como prencher eles)
 -------------------------------------------------------------------------- */
