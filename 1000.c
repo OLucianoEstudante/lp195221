a850f7d063b38eebafd81d40f863b432141bbe35
OLucianoEstudante <luciano.netto@unifesp.br>
2026-04-16 12:17:41 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,9 +1,10 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Luciano de Andrade da Silva Netto
 Linguagem   : C
 Plataforma  : https://judge.beecrowd.com/pt/search
 Exercício   : 1000
-Data        : 15/04/2026
+Data        : 16/04/2026
 Objetivo    : Hello World! como output
 Aprendizado : a funcao printf
 -------------------------------------------------------------------------- */
